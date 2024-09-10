de111e99a3b776f5edb903bd65f1f5bce21f474b
ChadWynter <79537339+ChadWynter@users.noreply.github.com>
2024-09-09 20:23:17 -0500

    Revert "Add ascii"

--- FileEncrypt/AsciiCodes.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include <string>
-#include <unordered_map>
-using namespace std;
-
-extern unordered_map<string, int> codes = { {"0", 48 } ,{"1", 49} ,{"2", 50}};
--- FileEncrypt/main.cpp
+++ FileEncrypt/main.cpp
@@ -1,25 +1,13 @@
 #include <iostream>
 #include <string>
 #include "encrypt.h"
-#include <fstream>
 
 
 using namespace std;
 int main(int argc, char* argv[]) {
 	//convert file to ascii codes
-	string line;
-	string file;
-
-	ifstream myfile;
-	myfile.open(file);
-
-	while (getline(myfile, line)) {
-
-	}
-
 	
-	//check if public key p and q valid
-
+	//check if public key valid
 
 	//generate private key
 
