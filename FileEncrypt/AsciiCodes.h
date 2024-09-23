f33c57099ef76007b85445e984dc9b624481b5ee
ChadWynter <Wyntecha000@gmail.com>
2024-09-23 17:25:11 -0500

    adding unit tests

--- FileEncrypt/AsciiCodes.h
+++ FileEncrypt/AsciiCodes.h
@@ -1,6 +1,13 @@
 #pragma once
-#include <string>
 #include <unordered_map>
+#include <string>
+#include <vector>
 using namespace std;
 
-extern unordered_map<string, int> codes = { {"0", 48 } ,{"1", 49} ,{"2", 50}};
+void get_codes(string line, vector<vector<int>>* A_Codes) {
+	vector<int> temp;
+	for (int i = 0; i, line.size() - 1; i++) {
+		temp.push_back((int)line[i]);
+	}
+	A_Codes->push_back(temp);
+}
--- FileEncrypt/main.cpp
+++ FileEncrypt/main.cpp
@@ -3,22 +3,26 @@
 #include "encrypt.h"
 #include <fstream>
 #include <vector>
+#include "AsciiCodes.h"
 
 
 using namespace std;
+#ifdef _DEBUG
 int main(int argc, char* argv[]) {
 	//convert file to ascii codes
 	string line;
 	string file;
 	vector<string> lines;
+	vector<vector<int>> A_Codes;
 
 	ifstream myfile;
 	myfile.open(file);
-
+	int line_number = 0;
 	while (getline(myfile, line)) {
-		for (int i = 0; i < line.size(); i++) {
+		//convert each char into ascii representation and store in A_Codes
+		get_codes(line, &A_Codes);
 
-		}
+		line_number++;
 	}
 
 	
@@ -29,3 +33,21 @@ int main(int argc, char* argv[]) {
 
 
 }
+#else
+int main(int argc, char* argv[])
+{
+	//getCodes test
+	string line = "Testing";
+	vector<vector<int>> convert;
+
+	get_codes(line, &convert);
+
+	for (int i = 0; i < convert.size() - 1; i++) {
+		for (int j = 0; j < convert[i].size() - 1; j++) {
+			cout << convert[i][j] << ", ";
+		}
+	}
+
+}
+
+#endif
